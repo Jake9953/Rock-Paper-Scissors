0196ca979241458a9c090d13db90199211ae461c
Jake9953 <jacobwambua793@gmail.com>
2024-08-22 06:47:01 +0300

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const compChoice = ["rock", "paper", "scissors"];
 
 // Then here we generate our random number. The reason we multiply by two is because we want the the value 0 1 2, since in array we start in 0
+// Math.round - rounds the generated random number to a whole number because Math.random generated a number between 0 - 1
 const randomChoice = Math.round(Math.random() * 2);
 
 // This function now will randomly return one of the choices in compChoice array
