99376d6f33ece3d2ef7c1ad7f8ba8221899675eb
Jake9953 <jacobwambua793@gmail.com>
2024-08-22 07:01:44 +0300

    Function getHumanChoice works as expected.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // First we need to put our choice in an array
-const compChoice = ["rock", "paper", "scissors"];
+const compChoice = ["Rock", "Paper", "Scissors"];
 
 // Then here we generate our random number. The reason we multiply by two is because we want the the value 0 1 2, since in array we start in 0
 // Math.round - rounds the generated random number to a whole number because Math.random generated a number between 0 - 1
@@ -11,3 +11,14 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+
+// Human choice
+const humanChoice = prompt("Please Enter Rock, Paper Scissor: ");
+
+// Function to get human choice
+function getHumanChoice() {
+  return humanChoice;
+}
+
+console.log(getHumanChoice());
