883cfa3da22df6c0d0760729cf88d260f366a959
Jake9953 <jacobwambua793@gmail.com>
2024-08-25 10:39:18 +0300

    Refactored to work on the playRound Function using another angle.

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
 let humanScore = 0;
 let computerScore = 0;
-// First we need to put our choice in an array
-const compChoice = ["Rock", "Paper", "Scissors"];
+
+
+
 // Human choice
 const humanChoice = prompt("Please Enter Rock, Paper, Scissor: ");
-// Then here we generate our random number. The reason we multiply by two is because we want the the value 0 1 2, since in array we start in 0
-// Math.round - rounds the generated random number to a whole number because Math.random generated a number between 0 - 1
-const randomChoice = Math.round(Math.random() * 2);
-// This function now will randomly return one of the choices in compChoice array
-function getComputerChoice() {
-  return compChoice[randomChoice].toUpperCase();
-}
-console.log(getComputerChoice());
 
 // Function to get human choice
 function getHumanChoice() {
-  return humanChoice.toUpperCase();
+  return console.log("You chose:", humanChoice.toUpperCase());
 }
-console.log(getHumanChoice());
+getHumanChoice();
+
+
+
+// First we need to put our choice in an array
+const choices = ["Rock", "Paper", "Scissors"];
+
+// Then here we generate our random number. The reason we multiply by two is because we want the the value 0 1 2, since in array we start in 0
+// Math.round - rounds the generated random number to a whole number because Math.random generated a number between 0 - 1
+const randomChoice = Math.round(Math.random() * 2);
+const compChoice = choices[randomChoice];
+
+// This function now will randomly return one of the choices in compChoice array
+function getComputerChoice() {
+  return console.log("Computer chose:", compChoice.toUpperCase());
+
+}
+getComputerChoice();
 
 // This function will enable the game play a single round
-function playRound(humanChoice, compChoice) {
-  if (humanChoice === compChoice) {
-    console.log("Draw 🤝");
-  } else if (
-    (humanChoice === "Paper" && compChoice === "Rock") ||
-    (humanChoice === "Rock" && compChoice === "Scissors") ||
-    (humanChoice === "Scissors" && compChoice === "Paper")
-  ) {
-    console.log("You Win");
-    humanScore++;
-  } else {
-    console.log("You Lost!");
-    computerScore++;
-  }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
